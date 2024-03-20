5f2dbdc0c2cb58fb30ce056e596a798ba742d3ec
Callum Basnett <callumbasnett@gmail.com>
2024-03-20 14:36:31 +0000

    Got other components ported to next.js

--- app/page.js
+++ app/page.js
@@ -5,19 +5,16 @@ import "react";
 import Hero from "../src/hero-folder/Hero.jsx";
 import Hiw from "../src/how-it-works-folder/hiw.jsx";
 import Footer from "../src/footer-folder/footer.jsx";
+import Header from "../src/header-folder/header.jsx";
 
 export default function Home() {return (
   <>
-    <header>
-      <span>🔥 Fireplace Palace</span>
-    </header>
-
+  <Header />
     <main>
       <Hero />
       <Hiw />
     </main>
-
-    <Footer />
+  <Footer />
   </>
 );
 }
---
+++ src/header-folder/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header>
+    <span>🔥 Fireplace Palace</span>
+  </header>
+  );
+}
