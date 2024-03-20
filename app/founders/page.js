2625c53bb9b7a558bddb9e4b0130d6dba795af9a
Callum Basnett <callumbasnett@gmail.com>
2024-03-20 14:59:45 +0000

    tried unsuccessfully to get fonts working

--- app/founders/page.js
+++ app/founders/page.js
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import "./page.css";
-import "react";
+import React from "react";
 
-import Hero from ".../src/hero-folder/Hero.jsx";
-import Hiw from ".../src/how-it-works-folder/hiw.jsx";
-import Footer from ".../src/footer-folder/footer.jsx";
-import Header from ".../src/header-folder/header.jsx";
+import Hero from "../../src/hero-folder/Hero.jsx";
+import Hiw from "../../src/how-it-works-folder/hiw.jsx";
+import Footer from "../../src/footer-folder/footer.jsx";
+import Header from "../../src/header-folder/header.jsx";
 
 export default function Founders() {return (
   <>
- <h1>TEST</h1>
+    <Header />
     <main>
-      <Hero />
-      <Hiw />
+      
     </main>
   <Footer />
   </>
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
+import { Aleo } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+const aleo = Aleo({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={aleo.className}>{children}</body>
     </html>
   );
 }
