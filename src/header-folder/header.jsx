4662fb1f3000e0b1e940ecada13a54d1cd9c2243
mustie <mustafagcode@hotmail.com>
2024-03-20 16:42:24 +0000

    menu button added

--- src/header-folder/header.jsx
+++ src/header-folder/header.jsx
@@ -5,7 +5,14 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header>
-    <span>🔥 Fireplace Palace</span>
-  </header>
+      <span>🔥 Fireplace Palace</span>
+      <Image
+        id="menu-open-pic"
+        src="/menu-open-button.png"
+        width={30}
+        height={30}
+        alt="menu-button"
+      />
+    </header>
   );
 }
