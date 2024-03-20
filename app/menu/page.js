adb7c16dc2849c2f03cbbc2439334217c58af09a
Callum Basnett <callumbasnett@gmail.com>
2024-03-20 23:54:42 +0000

    Switch menu from being its own page to being a menu opened using states

--- app/menu/page.js
+++ app/menu/page.js
@@ -1,21 +1,26 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+// import React from "react";
+// import Image from "next/image";
+// import Link from "next/link";
+// import "./menu.css";
 
-export default function Menu() {
-  return (
-    <main>
-      <Image
-        id="menu-close-pic"
-        src="/menu-close-button.png"
-        width={30}
-        height={30}
-        alt="menu-button"
-      />
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/founders">Meet the Founders</Link>
-      </nav>
-    </main>
-  );
-}
+
+// export default function Menu() {
+//   return (
+//     <main>
+//       <button id="menu-close-button" onClick={() => window.history.back()}>
+//         <Image
+//           id="menu-close-pic"
+//           src="/menu-close-button.png"
+//           width={30}
+//           height={30}
+//           alt="menu-button"
+//         />
+//       </button>
+
+//       <nav>
+//         <div className='link'><Link href="/">Home</Link></div>
+//         <div className='link'><Link href="/founders">Meet the Founders</Link></div>
+//       </nav>
+//     </main>
+//   );
+// }
--- src/header-folder/header.jsx
+++ src/header-folder/header.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+'use client';
+import React, { useState } from 'react';
 import "./header.css";
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header>
       <span>🔥 Fireplace Palace</span>
-      <Image
-        id="menu-open-pic"
-        src="/menu-open-button.png"
-        width={30}
-        height={30}
-        alt="menu-button"
-      />
+      <button onClick={toggleMenu}>
+        <Image
+          id="menu-open-pic"
+          src="/menu-open-button.png"
+          width={30}
+          height={30}
+          alt="menu-button"
+        />
+      </button>
+      {isOpen && (
+        <nav>
+          <Link href="/">Home</Link>
+          <Link href="/founders">Meet the Founders</Link>
+          {/* Add more links as needed */}
+        </nav>
+      )}
     </header>
   );
 }
