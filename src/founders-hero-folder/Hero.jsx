74e7abc4f2292b9c3faa8791d297d63925ca7e25
mustie <mustafagcode@hotmail.com>
2024-03-20 15:48:05 +0000

    founders page progress

--- src/founders-hero-folder/Hero.jsx
+++ src/founders-hero-folder/Hero.jsx
@@ -5,30 +5,24 @@ import Image from "next/image";
 export default function FounderHero() {
   return (
     <>
-      <div className="hero">
-        <div className="hero-mobile-pic-container">
-          <Image
-            id="hero-mobile-pic"
-            src="/hero-mobile.png"
-            width={500}
-            height={500}
-            alt="hero-mobile"
-          />
-        </div>
+      <div className="orange-box-container">
+        <div className="orange-box">
+          <div className="orange-box-top">
+            Meet the artisans
+            <br />
+            behind our
+            <br />
+            masterpieces!
+          </div>
 
-        <div className="orange-box-container">
-          <div className="orange-box">
-            <div className="orange-box-top">
-              Discover the
-              <br />
-              Perfect Fireplace...
-            </div>
+          <div className="orange-box-middle">Mike and Mandy</div>
 
-            <div className="orange-box-bottom">
-              <div className="orange-box-bottom-left">Book consultation:</div>
-
-              <div className="orange-box-bottom-right">0121 345 6789</div>
-            </div>
+          <div className="orange-box-bottom">
+            <Image
+              id="hero-mobile-pic"
+              src="/founder-mike-and-mandy.png"
+              alt="hero-mobile"
+            />
           </div>
         </div>
       </div>
