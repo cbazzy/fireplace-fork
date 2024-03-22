5fae77405688e27d633d93192ecafc3ee788a404
teonkajkafon <catherinesarquis@gmail.com>
2024-03-22 02:10:56 +0000

    menu btn changes on state change

--- src/header-folder/header.jsx
+++ src/header-folder/header.jsx
@@ -1,36 +1,52 @@
-'use client';
-import React, { useState } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import "./header.css";
 import Image from "next/image";
 import Link from "next/link";
 
-
+//Initialise a state variable called menuToggle, set to false
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [menuToggle, setMenuToggle] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  // UseEffect is a React hook that runs when menuToggle state changes
+  useEffect(() => {
+    if (menuToggle) {
+      console.log("Menu opened");
+    }
+  }, [menuToggle]);
+
+  // This function is called when menu button is clicked
+  function handleClick() {
+    // Toggle the menuToggle state from true to false or vice versa
+    setMenuToggle(!menuToggle);
+  }
+
+  // var for path to the correct image based on the menuToggle state
+  const menuButtonImage = menuToggle
+    ? "/menu-close-button.png"
+    : "/menu-open-button.png";
 
   return (
     <header>
       <span>🔥 Fireplace Palace</span>
-      <button onClick={toggleMenu}>
+      <button onClick={handleClick}>
         <Image
-          id="menu-open-pic"
-          src="/menu-open-button.png"
+          className="menu-btn"
+          src={menuButtonImage}
           width={30}
           height={30}
           alt="menu-button"
         />
       </button>
-      {isOpen && (
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/founders">Meet the Founders</Link>
-          {/* Add more links as needed */}
-        </nav>
-      )}
+      {menuToggle ? (
+        <div className="mobile-menu">
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/founders">Meet the Founders</Link>
+            {/* Add more links as needed */}{" "}
+          </nav>
+        </div>
+      ) : null}
     </header>
   );
 }
