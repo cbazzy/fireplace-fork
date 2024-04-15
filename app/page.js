92fbe32c00a27d21f76244ec7a3e9fc0852fc2cd
Callum Basnett <callumbasnett@gmail.com>
2024-04-15 05:22:49 +0100

    Revert "attempting to fix font not working with Vercel"
    This reverts commit 8e5a955c5654b42896aa971536c39c1c9f30b547.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import "./page.css";
 import "react";
-import { Aleo } from "next/font/google";
 
 import Hero from "../src/hero-folder/Hero.jsx";
 import Hiw from "../src/how-it-works-folder/hiw.jsx";
@@ -12,13 +11,13 @@ import Trusted from "../src/trusted-folder/trusted.jsx";
 export default function Home() {
   return (
     <>
-      <main className="aleo.main">
-        <Header />
+      <Header />
+      <main>
         <Hero />
         <Trusted />
         <Hiw />
-        <Footer />
       </main>
+      <Footer />
     </>
   );
 }
